c7f861c252283f477b4d0de89a6602c8e6ed6593
abuyako <abuyako@gmail.com>
2020-02-06 23:20:29 +0300

    adding fuctionality to the button and set the score to 0

--- app.js
+++ app.js
@@ -1,8 +1,23 @@
-var scores, roundScore, activePlayer, dice;
+var scores, roundScore, activePlayer;
 
 scores = [0,0];
 roundScore = 0;
 activePlayer = 0;
 
-dice = Math.floor(Math.random() *6 ) + 1;
-document.querySelector('#current-0').textContent = dice;
+document.querySelector('.dice').style.display = 'none';
+
+document.getElementById('score-0').textContent = '0';
+document.getElementById('score-1').textContent = '0';
+document.getElementById('current-0').textContent = '0';
+document.getElementById('current-1').textContent = '0';
+
+document.querySelector('.btn-roll').addEventListener('click', function() {
+    //random number
+    var dice = Math.floor(Math.random() *6 ) + 1;
+
+    //display the result
+    var diceDOM = document.querySelector('.dice');
+    diceDOM.style.display = 'block';
+    diceDOM.src = 'dice-' + dice + '.png';
+});
+
