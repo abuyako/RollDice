1fb772b7b226265dceefbadc22dcff4278f28205
abuyako <abuyako@gmail.com>
2020-02-07 12:23:22 +0300

    checking if the player won the game and update it to global score UI

--- app.js
+++ app.js
@@ -40,8 +40,16 @@ document.querySelector('.btn-hold').addEventListener('click', function() {
     document.querySelector('#score-' + activePlayer).textContent = scores[activePlayer];
 
     // check if the player won the game
+    if ( scores[activePlayer] >= 10) {
+        document.querySelector('#name-' + activePlayer).textContent = "WINNER!";
+        document.querySelector('.dice').style.display = 'none';
+        document.querySelector('.player-' + activePlayer + '-panel').classList.add('winner');
+        document.querySelector('.player-' + activePlayer + '-panel').classList.remove('active');
+    }else {
+         //next player
     nextPlayer();
 
+    }
 });
 
 function nextPlayer() {
