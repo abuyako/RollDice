2dedf8ec87c95fe9cd16c146d3aded8c3d439986
abuyako <abuyako@gmail.com>
2020-02-08 14:26:27 +0300

    adding the state variable to fix it not playing of if is playing by using if statement

--- app.js
+++ app.js
@@ -1,46 +1,53 @@
-var scores, roundScore, activePlayer;
+var scores, roundScore, activePlayer, gamePlaying;
 
 init();
 
-document.querySelector('.btn-roll').addEventListener('click', function() {
-    //random number
-    var dice = Math.floor(Math.random() *6 ) + 1;
+document.querySelector('.btn-roll').addEventListener('click', function () {
+    function (gamePlaying) {
 
-    //display the result
-    var diceDOM = document.querySelector('.dice');
-    diceDOM.style.display = 'block';
-    diceDOM.src = 'dice-' + dice + '.png';
+        // random number
+        var dice = Math.floor(Math.random() * 6) + 1;
 
-    //update the round score if the rolled number is not a 1
-    if (dice !== 1) {
-        roundScore += dice;
-        document.querySelector('#current-' + activePlayer).textContent = roundScore;
-    }else {
-       //next player
-       nextPlayer();
+        //display the result
+        var diceDOM = document.querySelector('.dice');
+        diceDOM.style.display = 'block';
+        diceDOM.src = 'dice-' + dice + '.png';
+
+        //update the round score if the rolled number is not a 1
+        if (dice !== 1) {
+            roundScore += dice;
+            document.querySelector('#current-' + activePlayer).textContent = roundScore;
+        } else {
+            //next player
+            nextPlayer();
+        }
     }
 });
 
 
-document.querySelector('.btn-hold').addEventListener('click', function() {
-    // Add current score to GLOBAl score
-    scores[activePlayer] += roundScore;
+document.querySelector('.btn-hold').addEventListener('click', function () {
+    if (gamePlaying) {
+        // Add current score to GLOBAl score
+        scores[activePlayer] += roundScore;
 
-    // update the UI
+        // update the UI
 
-    document.querySelector('#score-' + activePlayer).textContent = scores[activePlayer];
+        document.querySelector('#score-' + activePlayer).textContent = scores[activePlayer];
 
-    // check if the player won the game
-    if ( scores[activePlayer] >= 10) {
-        document.querySelector('#name-' + activePlayer).textContent = "WINNER!";
-        document.querySelector('.dice').style.display = 'none';
-        document.querySelector('.player-' + activePlayer + '-panel').classList.add('winner');
-        document.querySelector('.player-' + activePlayer + '-panel').classList.remove('active');
-    }else {
-         //next player
-    nextPlayer();
+        // check if the player won the game
+        if (scores[activePlayer] >= 10) {
+            document.querySelector('#name-' + activePlayer).textContent = "WINNER!";
+            document.querySelector('.dice').style.display = 'none';
+            document.querySelector('.player-' + activePlayer + '-panel').classList.add('winner');
+            document.querySelector('.player-' + activePlayer + '-panel').classList.remove('active');
+            gamePlaying = false;
+        } else {
+            //next player
+            nextPlayer();
 
-    }
+        }
+
+    };
 });
 
 function nextPlayer() {
@@ -54,15 +61,16 @@ function nextPlayer() {
     document.querySelector('.player-1-panel').classList.toggle('active');
 
     document.querySelector('.dice').style.display = 'none';
-    
+
 };
 
-    document.querySelector('.btn-new').addEventListener('click', init);
+document.querySelector('.btn-new').addEventListener('click', init);
 
-    function init() {
-    scores = [0,0];
+function init() {
+    scores = [0, 0];
     roundScore = 0;
     activePlayer = 0;
+    gamePlaying = true;
 
     document.querySelector('.dice').style.display = 'none';
 
